5bc1fe96130ee682f68959abb655f5c1fffae63b
Welington Lima <welington.lima93@gmail.com>
2018-03-31 18:56:16 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/br/wjangoo/divertidamente/DivertidamenteApplication.java
@@ -0,0 +1,12 @@
+package br.wjangoo.divertidamente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivertidamenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivertidamenteApplication.class, args);
+	}
+}
